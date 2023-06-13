38deaa4b85dce3cf417e056bff47e5717318e771
Jakia Velazquez <jakiadavis84@gmail.com>
2023-06-13 23:29:33 +0000

    intro-lists

--- module4-intro-lists.py
+++ module4-intro-lists.py
@@ -1,3 +1,10 @@
 empty_list = []
 top_cities = ['New York City', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix']
 print(top_cities)
+
+top_cities = 'New York City'
+top_cities = []
+top_cities = ['New York City', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix']
+top_cities [0]
+top_cities [-5]
+top_cities [0:1]
