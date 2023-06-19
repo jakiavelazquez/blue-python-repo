f1af6a03b59a8ecc3397eca3d9e169c3ec395f43
Jakia Velazquez <jakiadavis84@gmail.com>
2023-06-19 17:15:09 +0000

    default-parameters

---
+++ module5-default-param-values.py
@@ -0,0 +1,12 @@
+print('Hello', 'How are you?', end='.', sep='-')
+
+def print_letter_count(text='This is the default string to search', letter='a'):
+    counter = 0
+    for char in text:
+        if char == letter:
+            counter += 1
+    print('Number of', letter, 'is', counter)
+    
+print_letter_count('How many letter a are here?')
+
+print_letter_count()
