6a72cc19f550b7fa7b9be47721895ebafe820ce4
Jakia Velazquez <jakiadavis84@gmail.com>
2023-06-19 16:08:44 +0000

    lists

---
+++ lists.py
@@ -0,0 +1,17 @@
+var = []
+
+print(type(var))
+
+print(type('What is this?'))
+
+print(type(2))
+
+print(type(3.14))
+
+var2= [151, 251, 386, 493, 649]
+
+print(var2)
+
+var2.append('009')
+
+print(var2)
