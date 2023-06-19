f3d2abfe252629aa2c7e80b1795830e3b04f858e
Jakia Velazquez <jakiadavis84@gmail.com>
2023-06-19 20:56:06 +0000

    exception-propagation

---
+++ module5-exception-propagation.py
@@ -0,0 +1,25 @@
+def get_day(user_info):
+    day = int(input('What is the day of your bday? '))
+    user_info.append(day)
+
+def get_month(user_info):
+    month = int(input('What is the month (1-12 of your bday? '))
+    user_info.append(month)
+
+def get_year(user_info):
+    year = int(input('What is the year of your bday? '))
+    user_info.append(year)
+
+def get_user_bday(user_info):
+    try:
+        get_day(user_info)
+        get_month(user_info)
+        get_year(user_info)
+        print('So your bday is', user_info)
+    except ValueError:
+        print('You entered incorrect data, bye!')
+        
+    
+print('Hi, I will collect some info about your bday!')
+user_info = []
+get_user_bday(user_info)
